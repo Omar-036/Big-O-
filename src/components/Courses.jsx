b4fd3c89bda52b458facef28627f8d945444a7bb
Omar Alhaj <oesam036@gmail.com>
2023-12-24 16:40:56 +0300

    error edit

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -34,9 +34,9 @@ function CourseList() {
 
   return (
     <div
-      className={`absolute top-[4rem]  hidden ${
-        childHover ? "block" : null
-      } group-hover:block bg-white border `}
+    // className={`absolute top-[4rem]  hidden ${
+    //   true ? "block" : null
+    // } group-hover:block bg-white border `}
     >
       <ul className=" relative ">
         {courses.map((course) => (
